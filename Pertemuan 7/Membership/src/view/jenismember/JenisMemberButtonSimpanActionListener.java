7a931d310d6be3839f6179a42644eb82eb716503
Muhammad Ridwan Rafansyah <116246754+Muhammadridwanrafansyah@users.noreply.github.com>
2024-12-02 07:18:53 +0700

    tugas 7 & 8

---
+++ Pertemuan 7/Membership/src/view/jenismember/JenisMemberButtonSimpanActionListener.java
@@ -0,0 +1,27 @@
+package PERTEMUAN_7.membership.src.view.jenismember;
+
+import java.awt.event.*;
+import java.util.UUID;
+import PERTEMUAN_7.membership.src.dao.JenisMemberDao;
+import PERTEMUAN_7.membership.src.model.JenisMember;
+
+public class JenisMemberButtonSimpanActionListener implements ActionListener {
+    private JenisMemberFrame jenisMemberFrame;
+    private JenisMemberDao jenisMemberDao;
+    
+    public JenisMemberButtonSimpanActionListener(JenisMemberFrame jenisMemberFrame, JenisMemberDao jenisMemberDao) {
+        this.jenisMemberFrame = jenisMemberFrame;
+        this.jenisMemberDao = jenisMemberDao;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String nama = this.jenisMemberFrame.getNama();
+        JenisMember jenisMember = new JenisMember();
+        jenisMember.setId(UUID.randomUUID().toString());
+        jenisMember.setNama(nama);
+        
+        this.jenisMemberFrame.addJenisMember(jenisMember);
+        this.jenisMemberDao.insert(jenisMember);
+    }
+}
